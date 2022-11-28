c495c394637aa763db589a835cd77882d375f767
SatCFDI <satcfdi@outlook.com>
2022-11-27 18:17:10 -0600

    added file for cfdi login

--- satcfdi/portal/__init__.py
+++ satcfdi/portal/__init__.py
@@ -122,6 +122,9 @@ class SATCfdiAUSession:
         assert res.status_code == 200
 
         action, data = get_post_form(res)
+        if not action.startswith('https://cfdiau.sat.gob.mx/'):
+            assert False, 'Ya se tiene una session'
+
         parts = urlparse(action)
         action = urlunparse((
             parts.scheme,
@@ -148,6 +151,15 @@ class SATCfdiAUSession:
                 'token': generate_token(self.signer, code=data['guid']),
                 'fert': self.signer.certificate.get_notAfter()[2:].decode(),
             }
+        )
+        assert res.status_code == 200
+
+        action, data = get_post_form(res)
+        ref_headers = request_ref_headers(res.request.url)
+        res = self.session.post(
+            url=action,
+            headers=DEFAULT_HEADERS | ref_headers,
+            data=data
         )
         assert res.status_code == 200
 
