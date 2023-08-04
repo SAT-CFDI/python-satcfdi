136ca2973217347c21ac73d7bf9844ee1fc18cba
SatCFDI <satcfdi@outlook.com>
2023-08-04 17:55:57 -0600

    updated

---
+++ tests/test_cfdi/comprobante40/cfdv40-donat11.xml.pretty.py
@@ -0,0 +1,45 @@
+{'Certificado': '',
+ 'CfdiRelacionados': [{'CfdiRelacionado': ['F4F09AEF-57F2-4BE0-A828-87D1A80ED61C'],
+                       'TipoRelacion': Code('09', 'Factura generada por pagos diferidos')}],
+ 'Complemento': {'Donatarias': {'FechaAutorizacion': datetime.date(1973, 4, 4),
+                                'Leyenda': 'Este comprobante ampara un '
+                                           'donativo, el cual será destinado '
+                                           'por la donataria a los fines '
+                                           'propios de su objeto social. En el '
+                                           'caso de que los bienes donados '
+                                           'hayan sido deducidos previamente '
+                                           'para los efectos del impuesto '
+                                           'sobre la renta, este donativo no '
+                                           'es deducible. La reproducción no '
+                                           'autorizada de este comprobante '
+                                           'constituye un delito en los '
+                                           'términos de las disposiciones '
+                                           'fiscales.',
+                                'NoAutorizacion': '123456ABC',
+                                'Version': '1.1'}},
+ 'Conceptos': [{'Cantidad': Decimal('1'),
+                'ClaveProdServ': Code('84111506', 'Servicios de facturación'),
+                'ClaveUnidad': Code('ACT', 'Actividad'),
+                'Descripcion': 'Descripcion',
+                'Importe': Decimal('0'),
+                'ObjetoImp': Code('01', 'No objeto de impuesto'),
+                'ValorUnitario': Decimal('0')}],
+ 'Emisor': {'Nombre': 'Esta es una demostración',
+            'RegimenFiscal': Code('622', 'Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras'),
+            'Rfc': ' AAA010101AAA'},
+ 'Exportacion': Code('03', 'Temporal'),
+ 'Fecha': datetime.datetime(2021, 12, 8, 23, 59, 59),
+ 'FormaPago': Code('02', 'Cheque nominativo'),
+ 'LugarExpedicion': '99999',
+ 'Moneda': Code('XXX', 'Los códigos asignados para las transacciones en que intervenga ninguna moneda'),
+ 'NoCertificado': '30001000000300023708',
+ 'Receptor': {'DomicilioFiscalReceptor': '99999',
+              'Nombre': 'Juanito Bananas De la Sierra',
+              'RegimenFiscalReceptor': Code('630', 'Enajenación de acciones en bolsa de valores'),
+              'Rfc': 'BASJ600902KL9',
+              'UsoCFDI': Code('P01', 'Por definir')},
+ 'Sello': '',
+ 'SubTotal': Decimal('0'),
+ 'TipoDeComprobante': Code('P', 'Pago'),
+ 'Total': Decimal('0'),
+ 'Version': '4.0'}
