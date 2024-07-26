14f49d2b3f0f05f37f6f410fec2af0e6faa0fbe4
SatCFDI <satcfdi@outlook.com>
2024-07-26 17:58:00 -0600

    Removed not used email manager

--- satcfdi/accounting/__init__.py
+++ satcfdi/accounting/__init__.py
@@ -2,4 +2,3 @@ from .formatters import SatCFDI
 from .process import invoices_print, payments_print,  invoices_confirmation_print
 from .process import invoices_export, payments_export, retentions_export, payments_retentions_export
 from .process import filter_invoices_iter, filter_payments_iter, filter_retenciones_iter, complement_invoices_data
-from .email import EmailManager
