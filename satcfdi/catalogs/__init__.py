ea86c7fd35e76b9b9893beb395296313cb99fab7
SatCFDI <satcfdi@outlook.com>
2024-08-13 18:05:20 -0600

    Added check_same_thread=False

--- satcfdi/catalogs/__init__.py
+++ satcfdi/catalogs/__init__.py
@@ -7,7 +7,7 @@ from ..models import Code
 current_dir = os.path.dirname(__file__)
 
 db_file = os.path.join(current_dir, "catalogs.db")
-conn = sqlite3.connect(db_file)
+conn = sqlite3.connect(db_file, check_same_thread=False)
 c = conn.cursor()
 
 
