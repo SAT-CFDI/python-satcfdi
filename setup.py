bb7bb79d1dacfa82017fb0a66b019ef2a9f66145
satcfdi <satcfdi@outlook.com>
2025-12-28 19:07:26 -0600

    Removed Python 3.14

--- setup.py
+++ setup.py
@@ -61,8 +61,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14"
+        "Programming Language :: Python :: 3.13"
     ],
     python_requires='>=3.11',
     package_dir={package: package_dir},
