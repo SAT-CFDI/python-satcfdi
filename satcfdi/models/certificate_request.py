56bf9c36f63c7085b4e03e7dfd143c1ae9b940b7
satcfdi <satcfdi@outlook.com>
2026-05-04 14:37:30 -0600

    Revert "clean up"
    This reverts commit 88876572c237664c88aed3850a563000636d0646.

--- satcfdi/models/certificate_request.py
+++ satcfdi/models/certificate_request.py
@@ -1,4 +1,6 @@
+from OpenSSL.crypto import FILETYPE_ASN1
 from cryptography import x509
+from OpenSSL import crypto
 
 
 class CertificateSigningRequest:
@@ -8,3 +10,7 @@ class CertificateSigningRequest:
     @classmethod
     def load(cls, request: bytes) -> 'CertificateSigningRequest':
         return cls(x509.load_der_x509_csr(request))
+
+    @classmethod
+    def load2(cls, request: bytes):
+        return crypto.load_certificate_request(FILETYPE_ASN1, request)
