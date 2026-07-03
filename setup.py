49d8e8ab0ce8e08d776e3244dca14ece00289b84
satcfdi <satcfdi@outlook.com>
2026-07-03 16:36:07 -0600

    Revert "Deprecated python 3.10"
    This reverts commit 7535b2ca

--- setup.py
+++ setup.py
@@ -59,12 +59,13 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: Spanish",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14"
     ],
-    python_requires='>=3.11',
+    python_requires='>=3.10',
     package_dir={package: package_dir},
     packages=packages,
     package_data={
